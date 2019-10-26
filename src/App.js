0a1a35e8776029d0c16115805de9d5542299b4eb
michalsh <mikikumi240@gmail.com>
2019-10-26 23:05:45 +0300

    first nisayon

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn from miki shosh
         </a>
       </header>
     </div>
